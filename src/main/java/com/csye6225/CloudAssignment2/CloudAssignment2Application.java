b1fd5758ecf207467988611dc67f6678b653df8b
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-10-02 14:49:30 -0400

    Assignment 2

---
+++ src/main/java/com/csye6225/CloudAssignment2/CloudAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.csye6225.CloudAssignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAssignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAssignment2Application.class, args);
+    }
+
+}
