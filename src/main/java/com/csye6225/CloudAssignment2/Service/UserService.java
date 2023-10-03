73d52ecb22d31e3eecd361aeecb3e04b5fef7f62
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-10-03 17:42:00 -0400

    added basic validations for the user login details

--- src/main/java/com/csye6225/CloudAssignment2/Service/UserService.java
+++ src/main/java/com/csye6225/CloudAssignment2/Service/UserService.java
@@ -2,6 +2,8 @@ package com.csye6225.CloudAssignment2.Service;
 
 import com.csye6225.CloudAssignment2.Model.User;
 import com.csye6225.CloudAssignment2.Repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.SqlFragmentAlias;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -30,6 +33,21 @@ public class UserService {
             String email=userData[2];
             String password=userData[3];
 
+            if ((firstname.length()<4 && firstname.length() > 20) ||(lastname.length()<4&& lastname.length() > 10)) {
+                log.error("First name and last name must be betweenm 4 and 20 characters  for user with email: {}", email);
+                continue;
+            }
+            if (!isValidEmail(email)) {
+                log.error("Invalid email format for user with email: {}", email);
+                continue;
+            }
+
+
+            if (password.length() < 8) {
+                log.error("Password must be at least 8 characters long for user with email: {}", email);
+                continue;
+            }
+
             User existing=userRepository.findByEmail(email);
             if(existing==null){
                 User user=new User();
@@ -41,6 +59,8 @@ public class UserService {
                 userRepository.save(user);
 
             }
+            else
+                log.error("User already present in the database with mail id :{}",email);
         }
     }
 
@@ -52,7 +72,13 @@ public class UserService {
             String line;
             br.readLine();
             while((line= br.readLine())!=null){
+
            String[] val=line.split(",");
+
+           if(val.length!=4){
+               log.error("Invalid data in CSV,Count is not equal to 4,{}",val.length);
+           }
+
             values.add(val);
 
             }
@@ -62,4 +88,8 @@ public class UserService {
         }
         return values;
     }
+    private static boolean isValidEmail(String email) {
+        String regex ="^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        return email.matches(regex);
+     }
 }
