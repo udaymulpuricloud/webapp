ed446ff411afc0bd6eb039c93fd858e51f5ab664
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-10-03 10:43:29 -0400

    updated the code for user entity and loading csv data

--- src/main/java/com/csye6225/CloudAssignment2/CloudAssignment2Application.java
+++ src/main/java/com/csye6225/CloudAssignment2/CloudAssignment2Application.java
@@ -1,5 +1,6 @@
 package com.csye6225.CloudAssignment2;
 
+import com.csye6225.CloudAssignment2.Service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudAssignment2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CloudAssignment2Application.class, args);
+        String filepath="/opt/user.csv";
+        UserService.LoadUsersFromCSV(filepath);
     }
+
 
 }
---
+++ src/main/java/com/csye6225/CloudAssignment2/Service/UserService.java
@@ -0,0 +1,64 @@
+package com.csye6225.CloudAssignment2.Service;
+
+import com.csye6225.CloudAssignment2.Model.User;
+import com.csye6225.CloudAssignment2.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private static UserRepository userRepository;
+
+    public static void LoadUsersFromCSV(String filepath){
+        List<String[]> csvData= parseCSV(filepath);
+
+        for(String[] userData:csvData){
+            String firstname=userData[0];
+            String lastname=userData[1];
+            String email=userData[2];
+            String password=userData[3];
+
+            User existing=userRepository.findByUsername(email);
+            if(existing==null){
+                User user=new User();
+                user.setFirstname(firstname);
+                user.setLastname(lastname);
+                user.setEmail(email);
+                String hashpassword = new BCryptPasswordEncoder().encode(password);
+                user.setPassword(hashpassword);
+                userRepository.save(user);
+
+            }
+        }
+    }
+
+    private static List<String[]> parseCSV(String filepath) {
+        List<String[]> values= new ArrayList<>();
+
+        try{
+            BufferedReader br= new BufferedReader(new FileReader(filepath));
+            String line;
+            while((line= br.readLine())!=null){
+           String[] val=line.split(",");
+            values.add(val);
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return values;
+    }
+}
