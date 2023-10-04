939ea89baf4cdafeb47092b90651bc9a0098d421
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-10-04 13:12:27 -0400

    Updated to Postegres

--- src/main/java/com/csye6225/CloudAssignment2/Model/Assignment.java
+++ src/main/java/com/csye6225/CloudAssignment2/Model/Assignment.java
@@ -1,9 +1,7 @@
 package com.csye6225.CloudAssignment2.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Date;
@@ -15,12 +13,20 @@ public class Assignment {
     private String name;
     private int points;
     private int num_of_attempts;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Date deadline;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Date assignmentCreated;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Date assignmentUpdated;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    private Account createdBy;
 
 }
