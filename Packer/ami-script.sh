fa47e5af13e179abca9f256b14d7c1d46020a3a6
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-10-17 11:39:53 -0400

    changess

--- Packer/ami-script.sh
+++ Packer/ami-script.sh
@@ -14,4 +14,4 @@ sudo apt-get install postgresql -y
 sudo systemctl start postgresql    # Start the PostgreSQL service
 sudo systemctl enable postgresql   # Enable PostgreSQL to start on boot
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Ud@R9603';"
-sudo -u postgres psql -c "CREATE DATABASE cloudmydb;"
+sudo -u postgres psql -c "CREATE DATABASE assignment3;"
