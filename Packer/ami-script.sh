c84d0cb1502d0d7e2e44366e85b650e8463a0baf
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-11-05 16:07:31 -0500

    code changes for cw configggg

--- Packer/ami-script.sh
+++ Packer/ami-script.sh
@@ -14,10 +14,6 @@
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i amazon-cloudwatch-agent.deb
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config\
--m ec2\
--c file:/opt/cloudwatch-config.json \
--s
 # Set up PostgreSQL
 #sudo apt-get install postgresql -y
 #
