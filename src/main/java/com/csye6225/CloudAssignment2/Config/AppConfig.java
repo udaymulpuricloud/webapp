e0d6716fe2db171cfa3ac84b18992ed81caa8af5
udaykiranreddymulpuri <mulpuri.u@northeastern.edu>
2023-11-28 18:20:32 -0500

    Code Change

---
+++ src/main/java/com/csye6225/CloudAssignment2/Config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.csye6225.CloudAssignment2.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public SnsClient snsClient(){
+//        AwsCredentialsProvider awsCredentialsProvider= ProfileCredentialsProvider.builder().profileName("demo").build();
+        return SnsClient.builder()
+                .region(Region.US_EAST_1)
+//                .credentialsProvider(awsCredentialsProvider)
+                .build();
+    }
+}
